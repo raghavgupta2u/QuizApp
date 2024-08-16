db3ed34764d7d834124f2da1bf4e08af41ad30f4
raghavgupta2u <raghav.gupta7rg@gmail.com>
2024-08-16 18:09:41 +0530

    QuizApp

---
+++ script.js
@@ -0,0 +1,103 @@
+const question = [
+    {
+        question: "Which is largest animal in the world?",
+        answers: [
+            { text: "shark", correct: false },
+            { text: "Blue Whale", correct: true },
+            { text: "Elephant", correct: false },
+            { text: "Giraffe", correct: false },
+        ]
+    },
+    {
+        question: "Which is the smallest continent in the world?",
+        answers: [
+            { text: "Asia", correct: false },
+            { text: "Australia", correct: true },
+            { text: "Arctic", correct: false },
+            { text: "Africa", correct: false },
+        ]
+    }
+];
+
+const questionElement = document.getElementById('question');
+const answerButtons = document.getElementById('answer-btn');
+const nextButton =document.getElementById('next-btn');
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function starQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = question[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + "." + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener('click' , selectAnswer)
+    });
+}
+
+function resetState(){
+   nextButton.style.display = "none";
+   while(answerButtons.firstChild){
+      answerButtons.removeChild(answerButtons.firstChild);
+   }
+}
+
+function selectAnswer(e){
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.correct === "true";
+  if(isCorrect){
+    selectedBtn.classList.add("correct");
+    score++;
+  }else{
+    selectedBtn.classList.add("incorrect");
+  }
+  Array.from(answerButtons.children).forEach(button =>{
+    if(button.dataset.correct === "true"){
+        button.classList.add("correct");
+    }
+    button.disabled = true;
+  });
+  nextButton.style.display = "block";
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scroed ${score} out of ${question.length}!`;
+    nextButton.innerHTML = "play Again";
+    nextButton.style.display  = "block";
+}
+
+function handleNextButton(){
+  currentQuestionIndex++;
+  if(currentQuestionIndex < question.length){
+    showQuestion();
+  }else{
+    showScore();
+  }
+}
+
+nextButton.addEventListener("click",()=>{
+    if(currentQuestionIndex< question.length){
+        handleNextButton();
+    }else{
+        starQuiz();
+    }
+})
+
+starQuiz();
